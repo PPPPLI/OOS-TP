c45e04dde1e1a7e9af904c887f567770eb98b0cb
Administrator <gitlab_admin_74beb7@example.com>
2024-10-11 16:46:00 +0200

    Commentaire ajouté

--- RentCar-8081/src/main/java/com/cloud/spring/tp1/controller/CarController.java
+++ RentCar-8081/src/main/java/com/cloud/spring/tp1/controller/CarController.java
@@ -51,20 +51,27 @@ public class CarController {
     public ResponseEntity<?> rentCar(@RequestBody Car car
     ) {
 
+        //Traiter d'abord le payment
         PaymentReply paymentRes = carPaymentService.rentPayment(car.getPlateNumber(), car.getPrice());
 
+        //Si le service Payment est échoué, retourner directement le message d'erreur
         if (!paymentRes.getIsSuccess()) {
             return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body("Please try later");
         }
 
+        //Si la donnée de Payment est insérée, stocker ensuite la commande de location
         int result = carService.rentCar(car.getBeginDate(),car.getEndDate(),car.getPlateNumber());
 
+        //En cas réussite, retourner le message de la réussite
         if(result == 1) {
 
             return ResponseEntity.ok("Rent successfully");
         }
 
+        //Sinon faire une compensation pour la table Payment
         carPaymentService.deletePayment(paymentRes.getPaymentId());
+
+        //Retourner le message d'erreur
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body("Please try later");
     }
 
